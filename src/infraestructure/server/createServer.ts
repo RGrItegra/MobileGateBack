50474762249ca72d04f9ad5c5147a7209655dce3
Mateo H <fdev@itegra.co>
2025-09-18 15:57:12 -0500

    feat: :sparkles: se agrego el cors

--- src/infraestructure/server/createServer.ts
+++ src/infraestructure/server/createServer.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import routerLogin from '../../application/routes/auth.route';
 import routerTicket from '../../application/routes/ticket.route';
 import userRouter from '../../application/routes/userRouter';
@@ -7,7 +8,12 @@ const createServer = () => {
     const app = express();
 
     app.use(express.json());
-
+        app.use(
+        cors({
+            origin: process.env.FRONTEND_URL || "http://localhost:3000", 
+            credentials: true, 
+        })
+    );
     //Rutas 
     app.use('/users', userRouter);
     app.use('/auth', routerLogin);
